fb96d83c37a0a90a1031f6f6aede591369e261d7
Trejon Stallsworth <trejonstallsworth@gmail.com>
2019-06-08 20:58:01 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,11 +2,10 @@ def my_select(collection)
   i = 0 
   myArray = []
   while i < collection.length
-  if yield (collection[i]) == true 
+  if yield (collection[i])
     myArray << collection[i]
   end 
   i = i + 1 
 end  
 myArray
 end 
-my_select(numbers) { |num| num.even? }
